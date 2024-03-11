0ea85fe0de92591982f442e16b095d51c7a44f4c
unknown <barzcorp.fx@gmail.com>
2024-03-11 13:43:09 +0100

    add card

--- components/Button.js
+++ components/Button.js
@@ -29,10 +29,17 @@ export const CircleButton = ({ imgUrl, handlePress, ...props }) => {
   );
 };
 
-export const RectButton = () => {
+export const RectButton = ({ minWidth, fontSize, handlePress, ...props }) => {
   return (
-    <View>
-      <Text>Button</Text>
-    </View>
+    <TouchableOpacity
+      style={{
+        backgroundColor: COLORS.primary,
+        borderRadius: SIZES.extraLarge,
+        minWidth: minWidth,
+        padding: SIZES.small,
+        ...props,
+      }}
+      onPress={handlePress}
+    ></TouchableOpacity>
   );
 };
